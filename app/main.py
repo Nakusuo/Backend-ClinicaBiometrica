4f7da3a17aabf81833b0d2ba00821fa04c440b18
DanielaFernandaOjedaArrelucea <daniela.oxeda@gmail.com>
2026-06-19 12:02:25 -0500

    Definidas rutas para citas, doctores, expedientes, pacientes y webhooks (integración con central) y añadirlas al Swagger

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
-from app.routers import auth
+from app.routers import (
+    auth,
+    pacientes,
+    doctores,
+    citas,
+    expedientes,
+    webhooks
+)
 
 app = FastAPI(
     title="API Clínica Telemedicina",
@@ -7,11 +14,17 @@ app = FastAPI(
     description="Backend clínico para la plataforma de telemedicina"
 )
 
-app.include_router(
-    auth.router,
-    prefix="/api/auth",
-    tags=["Auth"]
-)
+app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
+
+app.include_router(pacientes.router, prefix="/api/pacientes", tags=["Pacientes"])
+
+app.include_router(doctores.router, prefix="/api/doctores", tags=["Doctores"])
+
+app.include_router(citas.router, prefix="/api/citas", tags=["Citas"])
+
+app.include_router(expedientes.router, prefix="/api/expedientes", tags=["Expedientes"])
+
+app.include_router(webhooks.router, prefix="/api/webhooks", tags=["Webhooks"])
 
 @app.get("/")
 def root():
