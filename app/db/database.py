bbc5c76a9f4ad4c8752e84b0cc5595b49c58aecc
DanielaFernandaOjedaArrelucea <daniela.oxeda@gmail.com>
2026-06-19 12:21:56 -0500

    Modificación de credenciales a .env.example

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = (
-    "postgresql://postgres:123456@localhost:5432/telemedicina"
-)
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
